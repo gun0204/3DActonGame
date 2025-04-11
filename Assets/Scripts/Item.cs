391f067cd03ea92c1e592f5677ab402ebf691dea
gun0204 <pgw4823@naver.com>
2025-04-11 19:18:00 +0900

    8장

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -7,8 +7,26 @@ public class Item : MonoBehaviour
     public Type type;
     public int value;
 
+    Rigidbody rigid;
+    SphereCollider sphereCollider;
+
+    private void Awake()
+    {
+        rigid = GetComponent<Rigidbody>();
+        sphereCollider = GetComponent<SphereCollider>();
+    }
+
     private void Update()
     {
         transform.Rotate(Vector3.up * 10 * Time.deltaTime);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Floor")
+        {
+            rigid.isKinematic = true;
+            sphereCollider.enabled = false;
+        }
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -37,6 +37,7 @@ public class Player : MonoBehaviour
     bool isSwap;
     bool isReload;
     bool isFireReady = true;
+    bool isBorder;
 
     Vector3 moveVec;
     Vector3 dodgeVec;
@@ -93,7 +94,8 @@ public class Player : MonoBehaviour
         if (isSwap || isReload || !isFireReady)
             moveVec = Vector3.zero;
 
-        transform.position += moveVec * speed * (wDown ? 0.3f : 1f) * Time.deltaTime;
+        if(!isBorder)
+            transform.position += moveVec * speed * (wDown ? 0.3f : 1f) * Time.deltaTime;
 
         anim.SetBool("isRun", moveVec != Vector3.zero);
         anim.SetBool("isWalk", wDown);
@@ -280,6 +282,21 @@ public class Player : MonoBehaviour
             }
             Destroy(other.gameObject);
         }
+    }
+    void FreezeRotation()
+    {
+        rigid.angularVelocity = Vector3.zero;
+    }
+
+    void StopToWall()
+    {
+        Debug.DrawRay(transform.position, transform.forward * 5, Color.green);
+        isBorder = Physics.Raycast(transform.position, transform.forward, 5, LayerMask.GetMask("Wall"));
+    }
+    private void FixedUpdate()
+    {
+        FreezeRotation();
+        StopToWall();
     }
     void OnTriggerStay(Collider other)
     {
